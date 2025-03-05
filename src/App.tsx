9a80fb7c3d1576fe0dca713717870ebd4f970192
BalaPriyaMS <balapriya@intellicar.in>
2025-03-05 15:00:11 +0530

    shadcn setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Button>click me</Button>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
 }
 
 export default App;
